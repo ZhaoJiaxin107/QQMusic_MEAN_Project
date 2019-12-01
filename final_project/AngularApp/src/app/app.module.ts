588dfb17c944640cd178a97db66a870b27fc350e
ZhaoJiaxin107 <jzhao537@uwo.ca>
2019-12-01 12:26:47 -0500

    create a new account

--- final_project/AngularApp/src/app/app.module.ts
+++ final_project/AngularApp/src/app/app.module.ts
@@ -1,18 +1,29 @@
+//built-in
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+//components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
-
+import { SignUpComponent } from './user/sign-up/sign-up.component';
+//routes
+import { appRoutes } from './routes';
+import { from } from 'rxjs';
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ final_project/AngularApp/src/app/user/sign-up/sign-up.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../shared/user.service'
+import { NgForm } from '@angular/forms';
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css'],
+  providers:[UserService]
+})
+export class SignUpComponent implements OnInit {
+  emailRegex = /^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/;
+  showSuccessMessage:boolean;
+  serverErrorMessage:string;
+  constructor(private userService:UserService) { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(form:NgForm){
+    this.userService.postUser(form.value).subscribe(
+      res=>{
+        this.showSuccessMessage = true;
+        setTimeout(() => this.showSuccessMessage = false,4000);
+        this.resetForm(form);
+      },
+      err=>{
+        if(err.status === 422){
+          this.serverErrorMessage = err.error.join('<br/>');
+        }else{
+          this.serverErrorMessage = 'Something went wrong. Please contact admin.';
+        }
+      }
+    );
+  }
+
+  resetForm(form:NgForm){
+    this.userService.selectedUser = {
+      email:'',
+      password:''
+    };
+    form.resetForm();
+    this.serverErrorMessage='';
+  }
+}
