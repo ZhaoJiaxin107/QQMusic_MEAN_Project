c7046b5948544037f88901a9092a0b7f829448e6
ZhaoJiaxin107 <jzhao537@uwo.ca>
2019-12-04 17:19:05 -0500

    backend login from local and google
    backend login from local and google

--- final_project/server/controllers/user.controller.js
+++ final_project/server/controllers/user.controller.js
@@ -1,28 +1,74 @@
 const mongoose = require('mongoose');
 const passport = require('passport');
+const JWT = require('jsonwebtoken');
 const _= require('lodash');
+JWT_SECRETGOOGLE = 'codeworkrauthentication';
 
 const User = mongoose.model('User');
+signToken = user => {
+  return JWT.sign({
+    iss: 'CodeWorkr',
+    sub: user.id,
+    iat: new Date().getTime(), // current time
+    exp: new Date().setDate(new Date().getDate() + 1) // current time + 1 day ahead
+  }, JWT_SECRETGOOGLE);
+}
 
+module.exports.register =async (req,res,next) =>{
+    const fullname = req.body.fullname;
+    const email = req.body.email;
+    const password = req.body.passowrd;
+    // Check if there is a user with the same email
+    let foundUser = await User.findOne({ "local.email": email });
+    if (foundUser) { 
+      return res.status(403).json({ error: 'Email is already in use'});
+    }
 
-module.exports.register = (req,res,next) =>{
-    var user = new User();
-    user.fullname = req.body.fullname;
-    user.email = req.body.email;
-    user.password = req.body.password;
-    user.save((err,doc) => {
-      if(!err)
-        res.send(doc);  
-      else
-      {
-        if(err.code == 11000)
-          res.status(422).send(['Duplicate email address found.']);
-        else
-          return next(err);
+    // Is there a Google account with the same email?
+    foundUser = await User.findOne({ 
+      $or: [
+        { "google.email": email },
+      ] 
+    });
+    if (foundUser) {
+      foundUser.methods.push('local')
+      foundUser.local = {
+        fullname:fullname,
+        email: email, 
+        password: password
+      }
+      await foundUser.save()
+      // Generate the token
+      const token = signToken(foundUser);
+      // Respond with token
+      res.cookie('access_token', token, {
+        httpOnly: true
+      });
+      res.status(200).json({ success: true });
+    }
+
+  
+    // Create a new user
+    const newUser = new User({ 
+      method: 'local',
+      local: {
+        fullname:fullname,
+        email: email, 
+        password: password
       }
     });
 
-}
+    await newUser.save();
+
+    // Generate the token
+    const token = signToken(newUser);
+    // Send a cookie containing JWT
+    res.cookie('access_token', token, {
+      httpOnly: true
+    });
+    res.status(200).json({ success: true });
+  },
+
 
 module.exports.authenticate  = (req,res,next) =>{
   //call for passaprt authentication
