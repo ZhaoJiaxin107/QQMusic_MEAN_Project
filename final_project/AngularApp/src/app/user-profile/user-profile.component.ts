6eaaa17aa56f4c76a45bbb0d9156eee5bab21cc2
ZhaoJiaxin107 <jzhao537@uwo.ca>
2019-12-15 15:10:45 -0500

    update adding a review
    update adding a review

--- final_project/AngularApp/src/app/user-profile/user-profile.component.ts
+++ final_project/AngularApp/src/app/user-profile/user-profile.component.ts
@@ -93,7 +93,7 @@ export class UserProfileComponent implements OnInit {
 }
   onLogout(){
     this.userService.deleteToken();
-    this.router.navigate(['/login']);
+    this.router.navigate(['/home']);
   }
 
 }
